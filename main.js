8edd7e6e4a68b4c26368911405443fd7b93e410d
RedVeluvRena <renajoy2323@gmail.com>
2024-10-03 14:40:09 +0800

    popularRestaurants first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js';
+
+
+
+console.log("Hello world!");
